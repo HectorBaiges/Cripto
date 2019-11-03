d62b6377c87996fad8d7e77494eb0654ea5f8d06
Hector Baiges <hectorbaigessanchez@gmail.com>
2019-11-03 17:47:34 +0100

    Entrega

---
+++ Practica2/Python/Entrega/GF256.py
@@ -0,0 +1,59 @@
+exp = []
+log = []
+g = 0x02
+
+def GF_product_p(a,b):
+    result = 0
+    for i in range(8):
+        if ((b%2) == 0x01):
+            result = result ^a
+        aux = a << 1
+        if ((aux & 0x100) == 0x100):
+            aux ^= 0x1d
+        a = aux & 0xFF
+        b = b>>1
+        
+    return result & 0xff
+
+def GF_es_generador(a):
+    check = [False] * 256
+    check[0] = True
+    check[a] = True
+    k = 1
+    tmp = a
+    while (tmp != 1):
+        tmp = GF_product_p(tmp, a)
+        check[tmp] = True
+        k = k + 1
+    return (k==255)
+
+def GF_Tables():
+    global exp
+    global log
+    exp = [0x01]
+    log = [0] * 256
+    for i in range(2**8 -2):
+        ant = exp[i]
+        prod = GF_product_p(ant,g)
+        exp += [prod]
+        log[prod] = i+1
+    return exp, log
+
+def GF_product_t(a,b):
+    global exp
+    global log
+    ret = exp[(log[a] + log[b]) % 255]
+    return ret & 0xFF
+
+def GF_invers(a):
+    global exp
+    global log
+    ret = 0
+    if a != 0:
+        exp = log[a]
+        invExp = (0xFF - exp) % 255
+        ret = exp[invExp]
+    return ret
+
+#GF_Tables()
+#GF_product_t(2,0x0e)
---
+++ Practica2/Python/Entrega/ProvaGF.py
@@ -0,0 +1,83 @@
+import time
+import GF256 as gf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def timeForGF_product_T(a, b):
+    start = time.process_time()
+    gf.GF_product_t(a, b)
+    end = time.process_time()
+    return end - start
+
+def timeForGF_product_P(a, b):
+    start = time.process_time()
+    gf.GF_product_p(a, b)
+    end = time.process_time()
+    return end - start
+
+def GF_product_TvsGF_productP(b):
+    results = [[0.0, 0.0] for y in range(256)]
+    for i in range(1,256):
+        meanT = 0
+        for _ in range(10):
+            meanT += timeForGF_product_T(i, b)
+        meanT = meanT / 10
+
+        meanP = 0
+        for _ in range(10):
+            meanP += timeForGF_product_P(i, b)
+        meanP = meanP / 10
+
+        results[i][0] = meanT
+        results[i][1] = meanP
+    return results
+
+def graphResutlsAsBarChart(results, value):
+    n_groups = 256
+    
+    medians_table = [results[i][0] for i in range(256)]
+    
+    
+    mediants_poly = [results[i][1] for i in range(256)]
+    
+    fig, ax = plt.subplots()
+    
+    index = np.arange(n_groups)
+    bar_width = 0.75
+    
+    opacity = 1
+    
+    rects1 = plt.bar(index, medians_table, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='Product using tables')
+    
+    rects2 = plt.bar(index + bar_width, mediants_poly, bar_width,
+                     alpha=opacity,
+                     color='r',
+                     label='Product of polynomials')
+    
+    plt.xlabel('GF(256)')
+    plt.ylabel('Time (s)')
+    plt.title('Time comparison multiplying all elements in GF(256) by {}'.format(hex(value)))
+    plt.xticks(index + bar_width / 2, ())
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    gf.GF_Tables()
+    results = GF_product_TvsGF_productP(0x02)
+    graphResutlsAsBarChart(results, 0x02)
+    results = GF_product_TvsGF_productP(0x03)
+    graphResutlsAsBarChart(results, 0x03)
+    results = GF_product_TvsGF_productP(0x09)
+    graphResutlsAsBarChart(results, 0x09)
+    results = GF_product_TvsGF_productP(0x0B)
+    graphResutlsAsBarChart(results, 0x0B)
+    results = GF_product_TvsGF_productP(0x0D)
+    graphResutlsAsBarChart(results, 0x0D)
+    results = GF_product_TvsGF_productP(0x0E)
+    graphResutlsAsBarChart(results, 0x0E)
